97e859d0c6543463a08c55176cc34d607f9860cc
BENEDITO BISPO DE SOUZA NETO <021.824535@universidadesenaicimatec.edu.br>
2026-06-02 15:56:47 -0300

    fix: corrige endpoint da rota de cadastro no frontend

--- js/cadastro.js
+++ js/cadastro.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             try {
                 // REQUISIÇÃO REAL PARA O SEU SERVIDOR (Porta 4000)
-                const response = await fetch('http://localhost:4000/api/cadastro', {
+                const response = await fetch('https://ancora-backend-76ue.onrender.com/api/cadastro', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
